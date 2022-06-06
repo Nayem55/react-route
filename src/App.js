59c70d0458e3db96fc523e90954265524268b2ca
Nayem55 <nayem.sayed.web@gmail.com>
2022-06-07 00:26:36 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './components/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Wekcome to react-route</h1>
+     <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <h2>Please contact us for details </h2>
+        </div>
+    );
+};
+
+export default Contact;
